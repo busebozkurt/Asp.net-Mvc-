b5f6cf9c10784628e23cd8d3bd816a479f4798ab
Buse <bozkurtbuse2@gmail.com>
2019-02-21 09:10:03 +0300

    Öğrenci Kayıt Ve Listeleme
    Öğrenci Kayıt panelinden öğrencinin ve ders bilgilerinin alınması, bu  bilgilerin geçici olarak hafıza tutularak Öğrenci Listeleme sayfasında bu bilgilerin listelenmesi.

---
+++ OgrenciKayit/OgrenciKayit/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using OgrenciKayit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OgrenciKayit.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OgrenciKaydet(string Ad, string Soyad, string Ders, int Not)
+        {
+            if (HttpRuntime.Cache["Ogrenciler"] == null)
+            {
+                List<Ogrenci> OgrenciListesi = new List<Ogrenci>();
+                Ogrenci ogrenci = new Ogrenci();
+                ogrenci.Ad = Ad;
+                ogrenci.Soyad = Soyad;
+                ogrenci.Ders = Ders;
+                ogrenci.Not = Not;
+
+                OgrenciListesi.Add(ogrenci);
+                HttpRuntime.Cache["Ogrenciler"] = OgrenciListesi;
+            }
+            else
+            {
+                List<Ogrenci> OgrenciListesi = (List<Ogrenci>)HttpRuntime.Cache["Ogrenciler"];
+                Ogrenci ogrenci = new Ogrenci();
+                ogrenci.Ad = Ad;
+                ogrenci.Soyad = Soyad;
+                ogrenci.Ders = Ders;
+                ogrenci.Not = Not;
+
+                OgrenciListesi.Add(ogrenci);
+                HttpRuntime.Cache["Ogrenciler"] = OgrenciListesi;
+            }
+            return RedirectToAction("OgrenciListesi");
+        }
+
+        public ActionResult OgrenciListesi()
+        {
+            var model = (List<Ogrenci>)HttpRuntime.Cache["Ogrenciler"];
+            return View(model);
+        }
+    }
+}
---
+++ OgrenciKayit/OgrenciKayit/Models/Ogrenci.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OgrenciKayit.Models
+{
+    public class Ogrenci
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Ders{ get; set; }
+        public int Not { get; set; }
+    }
+}
